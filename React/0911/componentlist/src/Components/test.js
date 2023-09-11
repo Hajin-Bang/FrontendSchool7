f986d79b06ed930aa65ed575038537c507ace8b8
Hajin-Bang <hajin0619@naver.com>
2023-09-11 23:12:47 +0900

    📝 20230911 수업

---
+++ React/0911/componentlist/src/Components/test.js
@@ -0,0 +1,12 @@
+function test() {
+    console.log('test');
+}
+
+export function test2() {
+    console.log('test2');
+}
+
+export default test;
+
+//export default로 빼낸 것과 export로 빼낸 것의 차이를 app.js에서 확인해라. 
+//사용법이 다름
---
+++ React/0911/modestate/src/Components/MenuListItem/MenuListItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./MenuListItem.css"
+
+export default function MenuListItem(props) {
+
+    const onitemClick = () => {
+        props.setCurrentMood(props.mood)
+    }
+
+  return (
+    <li>
+        <button onClick={onitemClick} className="btn-item">기분이 : {props.mood}</button>
+    </li>
+  )
+}
